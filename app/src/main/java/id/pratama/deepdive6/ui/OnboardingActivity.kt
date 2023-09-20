15f1a3e1e1a6d2275356270b4f308306e35f5ac8
pratamawijaya <set.mnemonix@gmail.com>
2023-09-20 19:18:43 +0700

    init project deepdive week6

---
+++ app/src/main/java/id/pratama/deepdive6/ui/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package id.pratama.deepdive6.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.pratama.deepdive6.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
